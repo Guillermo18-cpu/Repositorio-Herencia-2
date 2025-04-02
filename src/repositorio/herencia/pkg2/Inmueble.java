2d151e2ae2e6128001259cc0e58f9bd0b36c05c2
Guillermo18-cpu <3126810804juan@gmail.com>
2025-04-02 15:46:35 -0500

    Se creo la clase Inmueble

---
+++ src/repositorio/herencia/pkg2/Inmueble.java
@@ -0,0 +1,23 @@
+package repositorio.herencia.pkg2;
+
+public class Inmueble {
+
+    protected int identificadorinmobiliario;
+    protected int area;
+    protected String direccion;
+    protected double precioVenta;
+
+    public Inmueble(int identificadorinmobiliario, int area, String direccion) {
+        this.identificadorinmobiliario = identificadorinmobiliario;
+        this.area = area;
+        this.direccion = direccion;
+    }
+
+    public void calcularPrecioVenta(double valorArea) {
+
+    }
+
+    public void imprimir() {
+
+    }
+}
