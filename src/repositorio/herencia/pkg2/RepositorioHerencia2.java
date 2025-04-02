70a16e2a5d81ee9b48b74403cb7fd14790ccba30
Guillermo18-cpu <3126810804juan@gmail.com>
2025-04-02 15:38:21 -0500

    Clase principal

---
+++ src/repositorio/herencia/pkg2/RepositorioHerencia2.java
@@ -0,0 +1,17 @@
+
+package repositorio.herencia.pkg2;
+
+/**
+ *
+ * @author Juan-
+ */
+public class RepositorioHerencia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
